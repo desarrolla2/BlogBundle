08e4934f9c21007ee8da6b9c086f7f189554b0c2
Radu Gribincea <radu.voda@yahoo.com>
2013-11-06 02:59:13 +0200

    fixed tinymce
    fixed tinymce

--- Resources/public/js/blog.js
+++ Resources/public/js/blog.js
@@ -1,7 +1,3 @@
 $(document).ready(function () {
-    tinyMCE.init({
-        menubar : false,
-        mode: 'textareas',
-        content_css: '/bundles/blog/css/textarea.css'
-    });
+    tinymce.init({selector:'textarea'});
 });
