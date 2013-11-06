753ad80635f951ebc383a6ddd9ebc304a8e4ea8b
Daniel González Cerviño <daniel.gonzalez@freelancemadrid.es>
2013-11-06 10:56:01 +0100

    [#1] - fix tinymce back

--- Entity/User.php
+++ Entity/User.php
@@ -10,18 +10,14 @@
 
 namespace Desarrolla2\Bundle\BlogBundle\Entity;
 
+use FOS\UserBundle\Entity\User as BaseUser;
 use Doctrine\ORM\Mapping as ORM;
-use Gedmo\Mapping\Annotation as Gedmo;
-
 
 /**
- * Class User
- *
- * @author Daniel González <daniel.gonzalez@freelancemadrid.es>
  * @ORM\Entity
  * @ORM\Table(name="fos_user")
  */
-class User
+class User extends BaseUser
 {
     /**
      * @ORM\Id
@@ -30,74 +26,9 @@ class User
      */
     protected $id;
 
-    /**
-     * @var string
-     *
-     * @ORM\Column(name="username", type="string", length=255, unique=true, nullable=false)
-     */
-    protected $username;
-
-    /**
-     * @var string
-     *
-     * @ORM\Column(name="email", type="string", length=255, unique=true, nullable=false)
-     */
-    protected $email;
-
-    /**
-     * The salt to use for hashing
-     *
-     * @var string
-     *
-     * @ORM\Column(name="salt", type="string", length=255, nullable=false)
-     */
-    protected $salt;
-
-    /**
-     * Encrypted password. Must be persisted.
-     *
-     * @var string
-     *
-     * @ORM\Column(name="password", type="string", length=255, nullable=false)
-     */
-    protected $password;
-
-    /**
-     * @var boolean
-     *
-     * @ORM\Column(name="enabled", type="boolean", nullable=false)
-     */
-    protected $enabled;
-
-    /**
-     * @var \DateTime
-     *
-     * @ORM\Column(name="last_login", type="datetime", nullable=true)
-     */
-    protected $lastLogin;
-
-    /**
-     * @var \DateTime $created_at
-     *
-     * @Gedmo\Timestampable(on="create")
-     * @ORM\Column(name="created_at", type="datetime")
-     */
-    protected $createdAt;
-
-    /**
-     * @var \DateTime $updated_at
-     *
-     * @Gedmo\Timestampable(on="update")
-     * @ORM\Column(name="updated_at", type="datetime")
-     */
-    protected $updatedAt;
-
-    /**
-     *
-     */
     public function __construct()
     {
-        $this->salt = base_convert(sha1(uniqid(mt_rand(), true)), 16, 36);
-        $this->enabled = false;
+        parent::__construct();
+        // your own logic
     }
 }
--- Resources/public/js/admin.js
+++ Resources/public/js/admin.js
@@ -1,11 +1,19 @@
-$(document).ready(function() {
+$(document).ready(function () {
     var base_url = window.location.protocol + "//" + window.location.host + "/";
     tinyMCE.init({
-        theme: "ribbon",
         keep_styles: false,
         preformatted: true,
-        document_base_url: base_url + "bundles/blog/js/tiny_mce/",
-        mode: 'textareas',
-        content_css: '/bundles/blog/css/textarea.css'
+        content_css: '/bundles/blog/css/textarea.css',
+        selector: "textarea",
+        theme: "modern",
+        plugins: [
+            "advlist autolink lists link image preview hr anchor pagebreak",
+            "searchreplace wordcount visualblocks visualchars code fullscreen",
+            "insertdatetime media nonbreaking save table contextmenu directionality",
+            "template paste textcolor"
+        ],
+        toolbar1: " undo redo | styleselect | bold italic forecolor backcolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image media preview",
+        image_advtab: true,
+
     });
 });
